69ba4a9861afc500a70c716f73c097348eeabb33
Andreas "Kungi" Klein <git@kungi.org>
2015-10-08 20:48:35 +0200

    Still triple prints

--- project.clj
+++ project.clj
@@ -4,6 +4,5 @@
   :dependencies [[compojure "1.3.1"]
                  [org.clojure/clojure "1.6.0"]
                  [reloaded.repl "0.1.0"]
-                 [ring/ring-defaults "0.1.2"]
                  [ring/ring-jetty-adapter "1.4.0"]]
   :source-paths ["dev/" "src/"])
